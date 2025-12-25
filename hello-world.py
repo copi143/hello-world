fd06e573213a552f46e95d6138bd7724e7118fce
copi143 <copi143@outlook.com>
2025-12-25 15:16:21 +0800

    update python

--- hello-world.py
+++ hello-world.py
@@ -6,6 +6,7 @@ import shutil
 import random
 import locale
 import unicodedata
+from typing import Callable
 
 lang = locale.getlocale()[0] or 'en_US'
 lang_code = lang.replace('_', '-').lower()
@@ -22,101 +23,64 @@ files = [f for f in os.listdir(code_dir) if os.path.isfile(os.path.join(code_dir
 
 char_delay = 0.05
 
+
+def render_frame(texts: str, pre_run: Callable, i: int) -> bool:
+  not_end = False
+  start_time = time.time()
+  columns = shutil.get_terminal_size().columns or 80
+  print('\033[H', end='')
+  pre_run()
+  for l, text in enumerate(texts.splitlines()):
+    text_list = []
+    for ch in text:
+      if ch == '\t':
+        text_list.append((8, ch))
+        text_list.extend([(0, '')] * 7)
+      elif unicodedata.east_asian_width(ch) in ['F', 'W']:
+        text_list.append((2, ch))
+        text_list.append((0, ''))
+      else:
+        text_list.append((1, ch))
+    chars = [' '] * columns
+    for j, (w, char) in enumerate(text_list):
+      if char == '':
+        continue
+      t = i / 100 - j * char_delay - l * 0.5
+      if t > 1:
+        if j + w - 1 < columns:
+          chars[j] = char
+          for k in range(1, w):
+            chars[j + k] = ''
+        continue
+      not_end = True
+      if t < 0:
+        continue
+      n = int((1 - t)**3 * columns)
+      if j + n + w - 1 < columns:
+        chars[j + n] = char
+        for k in range(1, w):
+          chars[j + n + k] = ''
+    print(''.join(chars))
+  sys.stdout.flush()
+  time.sleep(0.01 - (time.time() - start_time))
+  return not_end
+
+
 print('\033[?1049h\033[?25l', end='', flush=True)
 
 try:
-
-  i = -1
-  not_end = True
-  while not_end:
+  i = 0
+  while render_frame(texts, lambda: None, i):
     i += 1
-    not_end = False
-    start_time = time.time()
-    columns = shutil.get_terminal_size().columns or 80
-    print('\033[H', end='')
-    for l, text in enumerate(texts.splitlines()):
-      text_list = []
-      for ch in text:
-        if ch == '\t':
-          text_list.append((8, ch))
-          text_list.extend([(0, '')] * 7)
-        elif unicodedata.east_asian_width(ch) in ['F', 'W']:
-          text_list.append((2, ch))
-          text_list.append((0, ''))
-        else:
-          text_list.append((1, ch))
-      chars = [' '] * columns
-      for j, (w, char) in enumerate(text_list):
-        if char == '':
-          continue
-        t = i / 100 - j * char_delay - l * 0.5
-        if t > 1:
-          if j + w - 1 < columns:
-            chars[j] = char
-            for k in range(1, w):
-              chars[j + k] = ''
-          continue
-        not_end = True
-        if t < 0:
-          continue
-        n = int((1 - t)**3 * columns)
-        if j + n + w - 1 < columns:
-          chars[j + n] = char
-          for k in range(1, w):
-            chars[j + n + k] = ''
-      print(''.join(chars))
-    sys.stdout.flush()
-    time.sleep(0.01 - (time.time() - start_time))
-
   while True:
     random_file = random.choice(files)
     with open(os.path.join(code_dir, random_file)) as f:
       code = f.read().replace('$$$', texts.splitlines()[0])
     print('\033[H\033[2J', end='')
-    i = -1
-    not_end = True
-    while not_end:
+    i = 0
+    while render_frame(code, lambda: print(texts), i):
       i += 1
-      not_end = False
-      start_time = time.time()
-      columns = shutil.get_terminal_size().columns or 80
-      print('\033[H', end='')
-      print(texts)
-      for l, text in enumerate(code.splitlines()):
-        text_list = []
-        for ch in text:
-          if ch == '\t':
-            text_list.append((8, ch))
-            text_list.extend([(0, '')] * 7)
-          elif unicodedata.east_asian_width(ch) in ['F', 'W']:
-            text_list.append((2, ch))
-            text_list.append((0, ''))
-          else:
-            text_list.append((1, ch))
-        chars = [' '] * columns
-        for j, (w, char) in enumerate(text_list):
-          if char == '':
-            continue
-          t = i / 100 - j * char_delay - l * 0.5
-          if t > 1:
-            if j + w - 1 < columns:
-              chars[j] = char
-              for k in range(1, w):
-                chars[j + k] = ''
-            continue
-          not_end = True
-          if t < 0:
-            continue
-          n = int((1 - t)**3 * columns)
-          if j + n + w - 1 < columns:
-            chars[j + n] = char
-            for k in range(1, w):
-              chars[j + n + k] = ''
-        print(''.join(chars))
-      sys.stdout.flush()
-      time.sleep(0.01 - (time.time() - start_time))
     time.sleep(1)
-
 except:
   pass
 
